d939fed18df08b0ce96b7157e6a7cb5905d70e51
koscar44 <74256675+koscar44@users.noreply.github.com>
2021-05-11 18:05:47 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+<script>
+document.querySelector("#Bouton").onclick = function() { 
+document.querySelector("#tonDiv").style.display=(window.getComputedStyle(document.querySelector('#tonDiv')).display=='none') ? "block" : "none"; 
+}
+</script>
